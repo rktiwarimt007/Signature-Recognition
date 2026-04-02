564b8806a57c982837e7ea05b3d1a8e998f2b5f5
Rahul Kumar Tiwari <r.k.tiwari.mt007@gmail.com>
2026-04-03 01:11:39 +0530

    Juputer Notebook done

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Rahul Kumar Tiwari",
+    author_email="r.k.tiwari.mt007@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+    
+)
